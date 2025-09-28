68cfb428767b7b5404715a5ab703a76a64e62020
sweviceroy <113086422+sweviceroy@users.noreply.github.com>
2025-09-28 02:23:36 +0200

    Just some test about bmp and png hexdecoding with small 10x10 sample images

--- ITHSExerciseMetaDataReader/Program.cs
+++ ITHSExerciseMetaDataReader/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Hex dump demo (Q to quit)");
+
+            string imagesDir = Path.Combine(Directory.GetCurrentDirectory(), "images");
+            if (!Directory.Exists(imagesDir))
+            {
+                Console.WriteLine("No 'images' folder found!");
+                return;
+            }
+
+            string[] files = Directory.GetFiles(imagesDir);
+            if (files.Length == 0)
+            {
+                Console.WriteLine("No files in 'images' folder.");
+                return;
+            }
+
+            string file = files[1];
+            Console.WriteLine($"Reading file: {Path.GetFileName(file)}\n");
+
+            byte[] data = File.ReadAllBytes(file);
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.Write($"{data[i]:X2} ");
+                if ((i + 1) % 16 == 0)
+                    Console.WriteLine();
+            }
+
+            Console.WriteLine("\n\nPress Q to quit, any other key to refresh...");
+            var key = Console.ReadKey(true);
+            if (key.Key == ConsoleKey.Q) break;
+        }
+    }
+}
--- ITHSExerciseMetaDataReader/obj/Debug/net8.0/ITHSExerciseMetaDataReader.AssemblyInfo.cs
+++ ITHSExerciseMetaDataReader/obj/Debug/net8.0/ITHSExerciseMetaDataReader.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITHSExerciseMetaDataReader")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+255f7a66afd7781e5ffabd79a814fd736a37b7ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITHSExerciseMetaDataReader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITHSExerciseMetaDataReader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
